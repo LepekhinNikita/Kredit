fbbe3f803eae39f7e0396ff25863f448cbbda1ab
Lepehin Nikita <Lepekhinnikita@gmail.com>
2023-01-22 18:34:15 +0400

    changes code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,6 @@ public class CreditPaymentService {
     public double calculate(double AmountCredit, double percent, double time) {
         double PercentMouth = ((percent / 12) / 100);
 
-        double payment = (AmountCredit * PercentMouth * Math.pow((1 + PercentMouth),time) / ((Math.pow((1 + PercentMouth),time)) - 1));
-        return payment;
+        return (AmountCredit * PercentMouth * Math.pow((1 + PercentMouth), time) / ((Math.pow((1 + PercentMouth), time)) - 1));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double payment = service.calculate(1_000_000,9.99,36);
-        int Amount = (int)payment;
+        double payment = service.calculate(1_000_000, 9.99, 36);
+        int Amount = (int) payment;
         System.out.println("Ежемесячный платеж = " + Amount);
     }
 }
